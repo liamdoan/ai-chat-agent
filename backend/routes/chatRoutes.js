2835d48b3ac4da33c675730b2fd9d3c265dbaf57
liamdoan <lamdoan9898@gmail.com>
2025-05-15 01:48:01 +0300

    create routes for controllers

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getChatList } = require("../controllers/chat-controller/getChatListController");
+const { createNewChatThread, getContentOfSingleChat, sendMessagesToCurrentChat } = require("../controllers/chat-controller/singleChatController");
+const router = express.Router();
+
+router.get("/chats", getChatList);
+router.post("/chats", createNewChatThread);
+router.get("/chats/:id", getContentOfSingleChat);
+router.put("/chats/:id", sendMessagesToCurrentChat); 
+
+module.exports = router;
