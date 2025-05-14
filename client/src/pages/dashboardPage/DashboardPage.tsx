b8a4c0ab4684f443fd9003ac2ed59e6dfb957664
liamdoan <lamdoan9898@gmail.com>
2025-05-14 12:36:19 +0300

    base for Dashboard page, newForm is quite similar to one in newPrompt component, research the ability to reuse

--- client/src/pages/dashboardPage/DashboardPage.tsx
+++ client/src/pages/dashboardPage/DashboardPage.tsx
@@ -1,18 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import "./DashboardPage.css"
 import { useFetchChatListContext } from "../../utils/context/fetchChatListContext";
+import { useState } from "react";
 
 const DashboardPage = () => {
     //add userId later when integrating AUTH model into this project
     const userId = "12345";
     const navigate = useNavigate();
-    const {fetchChatList} = useFetchChatListContext()
+    const {fetchChatList} = useFetchChatListContext();
+    const [prompts, setPrompts] = useState<string | undefined> ("");
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
 
-        const text = e.target.text.value;
-        if (!text) return;
+        if (!prompts) return;
 
         try {
             const response = await fetch("http://localhost:5000/api/chats", {
@@ -21,7 +22,7 @@ const DashboardPage = () => {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({userId: userId ,text: text})
+                body: JSON.stringify({userId: userId ,text: prompts})
             })
 
             if (!response.ok) {
@@ -37,6 +38,11 @@ const DashboardPage = () => {
             console.error("Something is wrong, cant create a new chat.", error)
         }
     }
+
+    const handleExpandTextareaHeight = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        e.target.style.height = "auto";
+        e.target.style.height = `${e.target.scrollHeight}px`;
+    };
 
     return (
         <div className='dashboardPage'>
@@ -56,11 +62,18 @@ const DashboardPage = () => {
                 </div>
             </div>
             <div className="formContainer">
-                <form action="" onSubmit={handleSubmit}>
-                    <input type="text" name="text" placeholder='Ask me something...'/>
-                    <button>
-                        ^
-                    </button>
+                <form className="newForm" onSubmit={handleSubmit}>
+                    <textarea
+                        placeholder="Ask me something..."
+                        value={prompts}
+                        onChange={e => setPrompts(e.target.value)}
+                        onInput={handleExpandTextareaHeight}
+                    ></textarea>
+                    <div className="buttons">
+                        <button  className="submit-button" disabled={!prompts}>
+                            <img src="/up-arrow-icon.svg" alt="" />
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>
