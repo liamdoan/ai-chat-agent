d899e221f80430024dc3e08c4740f87624725c14
liamdoan <lamdoan9898@gmail.com>
2025-05-20 12:15:44 +0300

    starting point for main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './pages/homepage/Homepage.tsx';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.tsx';
+import DashboardPage from './pages/dashboardPage/DashboardPage.tsx';
+import ChatPage from './pages/chatPage/ChatPage.tsx';
+import RootLayout from './layouts/rootLayout/RootLayout.tsx';
+import SignupPage from './pages/signupPage/SignupPage.tsx';
+import { FetchChatListProvider } from './utils/context/fetchChatListContext.tsx';
+
+const router = createBrowserRouter([
+    {
+        element: <RootLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Homepage />,
+            },
+            {
+                path: "/sign-up/*",
+                element: <SignupPage />,
+            },
+            {
+                element: <DashboardLayout />,
+                children: [
+                    {
+                        path: "/dashboard",
+                        element: <DashboardPage />,
+                    },
+                    {
+                        path: "/dashboard/chats/:id",
+                        element: <ChatPage />,
+                    },
+                ],
+            },
+        ],
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <FetchChatListProvider>
+            <RouterProvider router={router} />
+        </FetchChatListProvider>
+    </StrictMode>,
 )
