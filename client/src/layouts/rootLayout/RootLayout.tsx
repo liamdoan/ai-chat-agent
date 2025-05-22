f585fffc35dad84de8644e368f05e6866b6beda9
liamdoan <lamdoan9898@gmail.com>
2025-05-22 06:20:59 +0300

    fix layout for HomePage

--- client/src/layouts/rootLayout/RootLayout.tsx
+++ client/src/layouts/rootLayout/RootLayout.tsx
@@ -1,20 +1,30 @@
 
 import './rootLayout.css'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 const RootLayout = () => {
+    const location = useLocation();
+    const isDashboardPath = location.pathname.startsWith('/dashboard');
+
     return (
-      <div className='rootLayout'>
+        <div className='rootLayout'>
             <header className='header'>
-                <Link to="/">My LOGO</Link>
+                {isDashboardPath ? (
+                    <div>MODEL_OPTIONS</div>
+                ) : (
+                    <Link className='logo-link' to="/explore">
+                        <img className='logo' src="/ai-chat-icon.png" alt="logo" />
+                        <span>LD AI Agent</span>
+                    </Link>
+                )}
                 <div className="user">
-                    USER
+                    USER_PROFILE
                 </div>
             </header>
             <main>
                 <Outlet />
             </main>
-      </div>
+        </div>
     )
 }
 
--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -7,14 +7,14 @@ const Homepage = () => {
             <img src="/spiral.png" alt="" className='background-img'/>
             <div className="left">
                 <h1>Welcome to LD AI Agent</h1>
-                <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, quaerat?</h2>
-                <Link to="/dashboard">Get Started</Link>
+                <h2>Talk to AI. Discover knowledge. Create smarter.</h2>
+                <Link className='get-started-btn' to="/dashboard">Get Started</Link>
             </div>
             <div className="right">
                 <div className="imgContainter">
                     <div className="bg"></div>
                 </div>
-                <img src="ai-robot.png" alt="" className='img-ai-robot'/>
+                <img src="/ai-robot.png" alt="" className='img-ai-robot'/>
             </div>
         </div>
     )
